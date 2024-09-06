fc3f9e08db52f646f12f1628f993bc062b31a98d
JuliaZenchenko <zerom3@yandex.ru>
2024-09-06 17:33:39 +0800

    Third commit

--- scooterapi.py
+++ scooterapi.py
@@ -5,33 +5,30 @@ import urls
 
 class ScooterApi:
 
+    @staticmethod
     @allure.step('Логин курьера в системе')
-    #@staticmethod
     def login(body):
         return requests.post(urls.BASE_URL + urls.LOGIN_COURIER, data=body)
 
-
+    @staticmethod
     @allure.step('Создание заказа')
-    #@staticmethod
     def order(body):
         return requests.post(urls.BASE_URL + urls.ORDER, json=body)
 
+    @staticmethod
     @allure.step('Получение списка заказов')
-    #@staticmethod
     def get_list_of_orders():
         return requests.get(urls.BASE_URL + urls.LIST_OF_ORDERS)
 
+    @staticmethod
     @allure.step('Удаление курьера в системе')
-    #@staticmethod
     def delete_courier(body):
         response = ScooterApi.login(body)
         courier_id = response.json().get("id")
         requests.delete(f'{urls.BASE_URL}/{courier_id}')
 
-
-
-    @allure.step('Получение id курьера из системы')
     @staticmethod
+    @allure.step('Получение id курьера из системы')
     def courier_id(body):
         response = ScooterApi.login(body)
         return response.json().get("id")
@@ -48,8 +45,8 @@ class ScooterApi:
         if response.status_code == 201:
             return body
 
+    @staticmethod
     @allure.step('Создание и логин курьера')
-    #@staticmethod
     def create_and_login_courier(payload):
         requests.post(urls.BASE_URL + urls.CREATE_COURIER, data=payload)
         response = requests.post(urls.BASE_URL + urls.LOGIN_COURIER, data=payload)
